15f0a686b7392b018125cf4fd54e9c69e79992e6
Alberto Albeiro Chindoy Urbano <Albertochind04@gmail.com>
2021-03-18 23:01:08 -0500

    parcial l4g

---
+++ string.py
@@ -0,0 +1,10 @@
+cadenaPalabras = 'este es un texto el cual deben contar el numero de palabras que tiene, deben tener en cuenta, que algunas palabras se separa por un punto, y una coma, tambien hay que tener en cuenta, que las palabras escritas EN MAYUSCULAS y minusculas cuenta como una este. Texto'
+
+minusculas= cadenaPalabras.lower()
+lista = minusculas.split()
+print("cadena "+str(lista))
+
+for i in lista:
+    contador = lista.count(i)
+    print(i+" "+str(contador))
+
