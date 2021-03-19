35fdf9c91143470abfb36b6d5c670547c9e4973e
Alberto Albeiro Chindoy Urbano <Albertochind04@gmail.com>
2021-03-18 23:20:23 -0500

    quitando signos

--- string.py
+++ string.py
@@ -1,5 +1,10 @@
 cadenaPalabras = 'este es un texto el cual deben contar el numero de palabras que tiene, deben tener en cuenta, que algunas palabras se separa por un punto, y una coma, tambien hay que tener en cuenta, que las palabras escritas EN MAYUSCULAS y minusculas cuenta como una este. Texto'
 
+signos=", : ; . ! ¡"
+
+for r in signos:
+    cadenaPalabras=cadenaPalabras.replace(r," ")
+
 minusculas= cadenaPalabras.lower()
 lista = minusculas.split()
 print("cadena "+str(lista))
@@ -7,4 +12,3 @@ print("cadena "+str(lista))
 for i in lista:
     contador = lista.count(i)
     print(i+" "+str(contador))
-
